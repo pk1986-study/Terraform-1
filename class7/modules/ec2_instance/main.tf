4f2fa7fcc15511b9f2ef01eca81e467b44bb3875
pk1986-study <pravinsaber@gmail.com>
2026-04-29 10:55:59 +0530

    Update main.tf

--- class7/modules/ec2_instance/main.tf
+++ class7/modules/ec2_instance/main.tf
@@ -12,6 +12,12 @@ variable "instance_type" {
     description = "This is instance"
   
 }
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-ec2-key"
+  public_key = file("/Users/pk/.ssh/my-ec2-key.pub")
+}
+
 resource "aws_instance" "pkinstance" {
     ami = var.ami
     instance_type = var.instance_type
